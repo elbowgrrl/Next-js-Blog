fa60b6bcca04e2b34e8f045335f8e56d193eb262
Megan V <elbowgrrl@gmail.com>
2021-09-27 14:05:47 -0700

    adding posts data files to file system as well as static rendering buisness logic and compenents

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+//importing posts data from file system
+import { getSortedPostsData } from '../lib/posts'
+//passes props to home component before home component renders
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -14,6 +25,20 @@ export default function Home() {
           (This blog is a very simple sample. You can build your own {' '}
           <a href="https://nextjs.org/learn">Here</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
